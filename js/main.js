5e992e113c9a7674d653160ee8f130247440c880
DESKTOP-ULON2PO\gile <suvi.genije@gmail.com>
2021-01-28 23:39:38 +0100

    objekti i nasledjivanje

--- js/main.js
+++ js/main.js
@@ -1,97 +1,47 @@
-// task 1
+// task 5
+
+var person = {
+	name: "Jack",
+	age: 32,
+	married: true
+}
+
+var  superPerson = Object.create(person);
+
+superPerson.location = 'Belgrade';
+superPerson.number = 1233456;
+
+
+
+console.log(superPerson.age);
+
+//task 6
+
 
 var person = {
 	name: "Mike",
 	age: 28,
-	married: true
-};
-
-
-function one(a) {
-	a.age = 34;
-	delete a.married;
-	console.log(a);
-}
-
-one(person);
-
-
-//task 2
-
- person = {
-	name: "Jack",
-	age: 32,
-	married: true
-};
-
-function offspring(a){
-	if (!('children' in a)) {
-		a.children=[];
-		a.children[0]= {
-			name: "John",
-			age: 11,
-			gender: 'male'
-		};
-		a.children[1] ={
-			name: "Ann",
-			age:12,
-			gender: 'female'
-		};
-
+	married: true,
+	log: function() {
+		console.log(this.name, this.age);
 	}
+};
+
+person.log();
+
+
+// task 7
+
+
+var person = {};
+
+person.create = function (a, b, c) {
+	this.name = a;
+	this.age = b;
+	this.married =c;
 }
 
-offspring(person);
+person.create('Ann', 23,  false);
+
+
 console.log(person);
-
-//task 3
-
-var students = [ 
-   {
-       name: 'Mike',
-       age: 28,
-       passed: false
-   },
-   {
-       name: 'Anna',
-       age: 23,
-       passed: true
-   },
-   {
-       name: 'Jack',
-       age: 32,
-       passed: true
-   },
-];
-
-
-function passed(a) {
-	for(var i=0; i<a.length;i++){
-		if (a[i].passed) {console.log(a[i].name+" passed exam.");
-				}else { console.log(a[i].name+ " didn't pass exam");}
-
-	}
-}	
-
-passed(students);
-
-//task 4
-
-var names = [];
-var ages = [];
-var passed =[];
-
-function populate(a){
-	for(var  i=0; i<a.length;i++) {
-		names[i]=a[i].name;
-		ages[i]=a[i].age;
-		passed[i]=a[i].passed;
-
-	}
-
-}
-
-populate(students);
-console.log(names);
-console.log(ages);
-console.log(passed);
