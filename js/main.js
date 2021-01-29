2489e17d3f9b26330b6aed21f7471421b008f928
DESKTOP-ULON2PO\gile <suvi.genije@gmail.com>
2021-01-29 22:18:26 +0100

    vezbanje

--- js/main.js
+++ js/main.js
@@ -1,47 +1,108 @@
-// task 5
 
-var person = {
-	name: "Jack",
-	age: 32,
-	married: true
-}
-
-var  superPerson = Object.create(person);
-
-superPerson.location = 'Belgrade';
-superPerson.number = 1233456;
+// TASK 5:
 
 
 
-console.log(superPerson.age);
-
-//task 6
-
-
-var person = {
-	name: "Mike",
-	age: 28,
-	married: true,
-	log: function() {
-		console.log(this.name, this.age);
-	}
+var someData = {
+	name: "Peter",
+	lastName: "Dinklage",
+	status: "married"
 };
 
-person.log();
+someData.hasName = function () {
+	if ('name' in this) {
+		delete this.name;
+		console.log(this);
+		this.thirdMethod =function() {
+			this.name = 'Mike';
+			console.log(this);
+		}
+		this.thirdMethod();
 
-
-// task 7
-
-
-var person = {};
-
-person.create = function (a, b, c) {
-	this.name = a;
-	this.age = b;
-	this.married =c;
+}
 }
 
-person.create('Ann', 23,  false);
+someData.hasName();
+someData.name = 'Peter';
+console.log(someData);
 
 
-console.log(person);
+//task 1
+
+var data = [[1,2,3],[4,5,6],[7,8,9]]
+
+function loop(a) {
+	for (i=0;i<a.length;i++) {
+		for (j=0; j<a[i].length; j++){
+			console.log("At position "+i+", subposition "+j+", value is "+a[i][j])
+		}
+	}
+}
+
+loop(data);
+
+
+// task 2
+
+Object.prototype.method = function () {
+	console.log(this.name, this.age);
+
+}
+
+data = [ {
+	name:'Mike',
+	age:23
+},
+	{
+		name:'Suzzanne',
+		age:34
+	},
+	{
+		name: 'Peter',
+		age:44
+	},
+	{
+		name:'Ann',
+		age:12
+	},
+	{
+		name:'Michael',
+		age: 54
+	}
+];
+
+for (var i=0;i<data.length;i++)
+	{	data[i].method();}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
