d98cb07890dec19877dc40df22e804b5652ba94d
DESKTOP-ULON2PO\gile <suvi.genije@gmail.com>
2021-04-26 13:11:36 +0200

    es6Sintaksa

--- js/main.js
+++ js/main.js
@@ -1,138 +1,177 @@
-var app_id = '739cfaa5';
-var app_key = '197a5227d653c84d8174048f7515d2f5';
-var count = 10;
-var page= 0;
+const team = { 
+	teamLogo: "./img/header_logo.svg",
+	teamName: "Real Madrid",
+	players: 
 
-var input = document.querySelector('.keyword-input');
-var health = document.querySelector('select[name="health"]');
-var diet = document.querySelector('select[name="diet"]');
-var recipesCount =document.querySelector('.recipe-count-number');
-var ul= document.querySelector('ul');
+		[
+		{name: "Thibaut",
+		 lastName: "Courtois",
+		 number: 1,
+		 age: 28,
+		 position: "goalkeeper",
+		 image: "./img/ThibautCourtois.jpg"
+		} ,
 
-var search = document.querySelector('.search-button');
-var recipes = document.querySelector('#recipes');
-
-var calorieMin=document.querySelector('input[name="calorieMin"]')  ;
-var calorieMax=document.querySelector('input[name="calorieMax"]') ;
-
-var results=[];
-
-input.addEventListener('change', enableSearch);
-health.addEventListener('change', enableSearch);
-diet.addEventListener('change', enableSearch);
-calorieMin.addEventListener('change', enableSearch);
-calorieMax.addEventListener('change', enableSearch);
-search.addEventListener('click', function() {
-	queryData(url); });
-
-var url ; 
-
-
-
-function enableSearch() {
-	if ((input.value!=='') && (health.value !=='') && (diet.value !== '') && (calorieMin.value !== '') && (calorieMax.value !==''))
-		 {search.removeAttribute ('disabled');
-		 url  = "https://api.edamam.com/search?q=" +input.value +"&app_id=" + app_id + "&app_key=" + app_key + "&from=" +page +"&to="+ (page + count) + "&calories="+ calorieMin.value + "-"+ calorieMax.value+"&health=" + health.value + "&diet=" +diet.value;
+		{name: "Sergio",
+		 lastName: "Ramos",
+		 number: 4,
+		 age: 35,
+		 position: "center-back",
+		 image: "./img/SergioRamosGarcía.jpg" 
+		},
+		{name: "Raphaël",
+		 lastName: "Varane",
+		 number: 5,
+		 age: 27,
+		 position: "center-back",
+		 image: "./img/RaphaëlVarane.jpg" 
+		},
+		{name: "Marcelo ",
+		 lastName: "da Silva",
+		 number: 12,
+		 age: 32,
+		 position: "left-back",
+		 image: "./img/MarceloVieiradaSilva.jpg" 
+		},
+		{name: "Daniel  ",
+		 lastName: "Carvajal ",
+		 number: 2,
+		 age: 29,
+		 position: "right-back",
+		 image: "./img/DanielCarvajalRamos.jpg" 
+		},
+		{name: "Carlos ",
+		 lastName: "Casimiro ",
+		 number: 14,
+		 age: 29,
+		 position: "Defensive Midfield",
+		 image: "./img/CarlosHenriqueCasimiro.jpg" 
+		},
+		{name: "Federico  ",
+		 lastName: "Valverde  ",
+		 number: 15,
+		 age: 22,
+		 position: "center Midfield",
+		 image: "./img/FedericoSantiagoValverdeDipetta.jpg" 
+		},
+		{name: "Toni  ",
+		 lastName: "Kroos  ",
+		 number: 8,
+		 age: 31,
+		 position: "center Midfield",
+		 image: "./img/ToniKroos.jpg" 
+		},
+		{name: "Francisco  ",
+		 lastName: "Suárez  ",
+		 number: 22,
+		 age: 28,
+		 position: "Attaking Midfield",
+		 image: "./img/FranciscoRománAlarcónSuárez.jpg" 
+		},
+		{name: "Karim  ",
+		 lastName: "Benzema ",
+		 number: 9,
+		 age: 33,
+		 position: " Centre-Forward",
+		 image: "./img/KarimBenzema.jpg" 
+		},
+		{name: "Andriy",
+		 lastName: "Lunin",
+		 number: 13,
+		 age: 22,
+		 position: "goalkeeper",
+		 image: "./img/AndriyLunin.jpg" 
+		},
+		{name: "Éder ",
+		 lastName: "Militão",
+		 number: 3,
+		 age: 23,
+		 position: " Centre-Back",
+		 image: "./img/ÉderGabrielMilitão.jpg" 
+		},
+		{name: "Luka  ",
+		 lastName: "Modric",
+		 number: 10,
+		 age: 35,
+		 position: "Central Midfield",
+		 image: "./img/LukaModrić.jpg" 
+		},
+		{name: "Ferland   ",
+		 lastName: "Mendy",
+		 number: 23,
+		 age: 25,
+		 position: "Left-Back",
+		 image: "./img/FerlandMendy.jpg" 
+		},
+		{name: "Mariano   ",
+		 lastName: "Díaz",
+		 number: 24,
+		 age: 27,
+		 position: "Centre-Forward",
+		 image: "./img/MarianoDíazMejía.jpg" 
 		}
-		else search.disabled = true ;
+		]
+	
 }
 
- function addRecipe(data){
- 	var recipeElement = document.createElement('section');
- 	recipeElement.classList.add('recipe-element');
+const firstSquad = document.getElementById('first-squad');
+const substitutes = document.getElementById('substitutes');
+const sub = document.getElementById('sub');
 
 
- 	var image = document.createElement('img');
- 	image.setAttribute('src', data.recipe.image )
- 	
- 	var label = document.createElement('h3');
- 	label.classList.add('labels');
- 	label.textContent = data.recipe.label;
+const addLogo= ()=>{
+	const logo=document.createElement('img');
+	const header = document.querySelector('header');
 
+	logo.setAttribute('src', team.teamLogo);
+	header.prepend(logo);
 
- 	var labels = document.createElement('article');
- 	// labels.classList.add('labels');
- 	labels.classList.add('label');
- 	labels.innerHTML = data.recipe.healthLabels;
-
- 	label.appendChild(labels);
- 	
- 	var calories=document.createElement('div');
- 	calories.textContent= Math.round(data.recipe.calories/data.recipe.yield);
-  	calories.setAttribute('z-index',9);
-  	
-
- 	calories.classList.add('calories');
-
- 	recipeElement.appendChild(image);
- 	recipeElement.appendChild(label);
-	recipeElement.appendChild(calories);
- 	// recipeElement.appendChild(labels);
- 	recipes.appendChild(recipeElement);
- 	
- }
-
- function populate(arr) {
- 
-			recipes.innerHTML='';
-		ul.innerHTML="";
- 	// for (var i=0; i<count; i++){
-
- 	// 	addRecipe(arr[i]);}
- 	arr.forEach(function(e) {addRecipe(e)});
-		
-		addPagination(page);
-
- }
-
-
-function createLink(number) {
-	var li=document.createElement('li');
-	var a= document.createElement('a')
-	a.setAttribute ('value', "https://api.edamam.com/search?q=" +input.value +"&app_id=" + app_id + "&app_key=" + app_key + "&from=" +(10*number) +"&to="+ (10*number +count) + "&calories="+ calorieMin.value + "-"+ calorieMax.value+"&health=" + health.value + "&diet=" +diet.value);
-	a.innerHTML=number+1;
-	li.appendChild(a);
-	li.addEventListener('click', function() {page=number; queryData(a.getAttribute('value')) });
-	
-
-	ul.appendChild(li);
 }
 
-function addPagination(pages) {
-	if (pages < 2) { for (var i=0; i<5; i++){
-		createLink(i);}}
-		 else {for (var i=pages-2; i<pages+3; i++) { createLink(i);}
-		}
-				}
+const  getRandom = (arr) => ( Math.round(Math.random()* (arr.length-1)));
 
 
-function queryData(url) {
-	
-	var req = new XMLHttpRequest;
-	req.open ('GET', url);
-	req.onload =function() {  
-		
-		 recipesCount.innerHTML = JSON.parse(req.responseText).count;
-		 		recipes.innerHTML='';
-				ul.innerHTMl="";
-		// console.log(JSON.parse(req.responseText).hits[0]);
-		
-		  results = JSON.parse(req.responseText).hits;
- 			populate(results);
- 			// input.value='';
- 			 
+const  populate = () => {
+	while (team.players.length){
+	const randomPlayer = getRandom(team.players);
+	const player = createPlayer(team.players[randomPlayer]);
+	let container = team.players.length > 4 ? firstSquad : substitutes ;
+	container.appendChild(player);
+	team.players.splice(randomPlayer,1)
+}
+}
+
+const  createPlayer = (playerData) => {
+	const player = document.createElement('div');
+	const image= '<img src="' + playerData.image + '">';
+	const name = '<div> ' + playerData.number+ ' ' + playerData.name + ' '+ playerData.lastName + '</div>';
+	const age = '<div> age: ' + playerData.age + '</div>';
+	const position= '<div>' + playerData.position + '</div>';
+	player.innerHTML = image + name + age + position;
+
+	return player;
+}
+
+const  makeSubstitute = () => {
+	const firstSquadPlayers = firstSquad.querySelectorAll('div');
+	const substitutePlayers = substitutes.querySelectorAll('div');
+
+	const firstSquadNumber = getRandom(firstSquadPlayers);
+	const substituteNumber = getRandom(substitutePlayers);
+	const firstPlayer = firstSquadPlayers[firstSquadNumber];	
+	const substitutePlayer = substitutePlayers[substituteNumber];
+	const previousSubsitutePlayer = substitutePlayer.previousSibling;
+
+	const nextSubsitutePlayer = substitutePlayer.nextSibling;
+
+	firstPlayer.before(substitutePlayer);
+	previousSubsitutePlayer ? previousSubsitutePlayer.after(firstPlayer) : nextSubsitutePlayer.before(firstPlayer);
+
+}
 
 
+addLogo();
+populate();
+// setInterval(makeSubstitute, 6000 );
 
-	};
-
-
-	req.send();
-
- }
-
-
- 
-
-
- //https://api.edamam.com/search?q=chicken&app_id=739cfaa5&app_key=197a5227d653c84d8174048f7515d2f5&from=0&to=3&calories=591-722&health=vegan
+sub.addEventListener('click', makeSubstitute);
